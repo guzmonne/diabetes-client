ac7abcb95e721262333923c00e23497f0d866daf
Guzman Monne <guzmonne@hotmail.com>
2017-08-10 16:21:38 -0300

    started working on a LineChart component

---
+++ src/components/common/LineChart/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import T from 'prop-types';
+
+const LineChart = () => (
+  
+);
+
+LineChart.propTypes = {
+  
+};
+
+LineChart.defaultProps = {
+  
+};
+
+export default LineChart;
