75fe8effa1e4abab0f7e04f8171af522f97c6e80
Guzman Monne <guzmonne@hotmail.com>
2017-08-14 23:11:05 -0300

    updated button

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,74 +1,17 @@
 import './style.css';
 import React from 'react';
 import T from 'prop-types';
-import {TimelineMax} from 'gsap';
-import omit from 'lodash/omit.js';
+import withRipple from '../Ripple/';
 
-const RIPPLE_DURATION = 300;
-
-class Button extends React.Component {
-  constructor() {
-    super();
-    // Bind functions.
-    this.handleClick = this.handleClick.bind(this);
-    // Initial state.
-    this.state = {
-      width: 0,
-      height: 0,
-    };
-  }
-
-  handleClick(e) {
-    const elem = this.circle,
-      tl = new TimelineMax(),
-      x = e.nativeEvent.offsetX,
-      y = e.nativeEvent.offsetY,
-      w = e.target.offsetWidth,
-      h = e.target.offsetHeight,
-      offsetX = Math.abs((w / 2) - x),
-      offsetY = Math.abs((h / 2) - y),
-      deltaX = (w / 2) + offsetX,
-      deltaY = (h / 2) + offsetY,
-      scaleRatio = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-    this.setState({
-      width: w,
-      height: h,
-    });
-    tl.fromTo(elem, RIPPLE_DURATION / 1000, {
-      x: x,
-      y: y,
-      transformOrigin: '50% 50%',
-      scale: 0,
-      opacity: 1,
-    }, {
-      scale: scaleRatio,
-      opacity: 0,
-    });
-    if (this.props.onClick){
-      e.persist();
-      setTimeout(() => this.props.onClick(e), RIPPLE_DURATION);
-    }
-  } 
-
-  render() {
-    const {children, type, block, ...props} = this.props;
-    return (
-      <div data-component="Button">
-        <button data-button
-          data-block={block}
-          data-type={type}
-          onClick={this.handleClick}
-          {...omit(props, 'onClick')}>
-          {children}
-        </button>
-        <svg data-ripple
-          viewBox={`0 0 ${this.state.width} ${this.state.height}`}>
-          <circle ref={c => this.circle = c} cx="1" cy="1" r="1" />
-        </svg>
-      </div>
-    );
-  }
-}
+const Button = ({block, type, circle, children, ...props}) => (
+  <button data-component="Button"
+    data-block={block}
+    data-type={type}
+    data-circle={circle}
+    {...props}>
+    {children}
+  </button>
+)
 
 Button.propTypes = {
   type: T.oneOf(['default', 'success', 'warning', 'danger', 'info']),
@@ -80,4 +23,4 @@ Button.defaultProps = {
   block: false,
 };
 
-export default Button
+export default withRipple(Button);
