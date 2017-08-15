8fbdb4a6f8ce9d7fc23be4eb2ba2dcbdb549f46d
Guzman Monne <guzmonne@hotmail.com>
2017-08-14 23:11:52 -0300

    updated icon circle button

--- src/components/common/IconCircleButton/index.js
+++ src/components/common/IconCircleButton/index.js
@@ -1,26 +1,23 @@
 import './style.css';
 import React from 'react';
 import T from 'prop-types';
-import CircleButton from '../CircleButton/';
-import AngleLeft from './icons/AngleLeft.js';
-import Times from './icons/Times.js';
+import Icon from '../Icon/';
+import Button from '../Button/';
 
-const ICONS = {
-  AngleLeft,
-  Times,
-};
-
-const IconCircleButton = ({diameter, icon, padding, ...props}) => (
-  <div data-component="IconCircleButton" style={{
-    width: diameter,
-    height: diameter
-  }}>
-    <CircleButton diameter={diameter} {...props} />
-    {React.createElement(ICONS[icon], {
-      width: diameter - padding,
-      height: diameter - padding,
-      fill: 'white',
-    })}
+const IconCircleButton = ({diameter, icon, padding, onClick, ...props}) => (
+  <div data-component="IconCircleButton">
+    <Button 
+      onClick={onClick}
+      circle
+      {...props}
+    >
+      <Icon type={icon}
+        onClick={onClick}  
+        width={diameter - padding}
+        height={diameter - padding}
+        fill="white" 
+      /> 
+    </Button>
   </div>
 );
 
